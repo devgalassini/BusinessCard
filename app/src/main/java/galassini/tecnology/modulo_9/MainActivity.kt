c48df0bc311b2f2e4e34706d2452d5bfe3b6a4a2
Priscila Galassini <priscilagalassini@gmail.com>
2024-05-13 15:56:51 -0300

    Exercicio do modulo 9 finalizado

---
+++ app/src/main/java/galassini/tecnology/modulo_9/MainActivity.kt
@@ -0,0 +1,12 @@
+
+package galassini.tecnology.modulo_9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
